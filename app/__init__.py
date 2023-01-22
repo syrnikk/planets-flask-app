ae4e1911426bf6ba45b2b083512f57758246fec4
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-01-22 20:33:06 +0100

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+from .auth import auth
+from .extensions import bcrypt, db, login_manager
+from .models import User
+from .views import views
+from config import Config
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    app.register_blueprint(views)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'secretkey'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
