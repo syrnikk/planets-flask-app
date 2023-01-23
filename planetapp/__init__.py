39d05f2fc3a32e22efa1da9e914cab93dbf60801
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-01-23 22:53:40 +0100

    Add error pages

--- planetapp/__init__.py
+++ planetapp/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .auth import auth
 from .extensions import bcrypt, db, login_manager
 from .views import views
 from config import Config
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 def create_app(config_class=Config):
@@ -16,6 +24,9 @@ def create_app(config_class=Config):
 
     app.register_blueprint(views)
     app.register_blueprint(auth)
+
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
 
     with app.app_context():
         db.create_all()
