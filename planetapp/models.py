9055df86465df2f09cfc8a27d585ff9a8d819dcf
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-02-06 18:44:36 +0100

    Fix model length

--- planetapp/models.py
+++ planetapp/models.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
     bio = db.Column(db.String)
     settings = db.relationship("Settings", uselist=False, back_populates="user")
